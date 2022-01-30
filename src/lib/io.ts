8109749b5c92cc91a67617518caa4cac1a1dbea5
Cfp <cfp@cfp.gotdns.ch>
2022-01-30 20:06:23 +0100

    Add input() to IO module

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -1,4 +1,8 @@
 import { Variable } from "../Variable";
+import * as promptsync from "prompt-sync";
+const prompt = promptsync({
+    sigint: true
+});
 
 export function log(stream, v?: any): void {
     if(v === undefined) {
@@ -18,4 +22,16 @@ export function print(v?: any): void {
 
 export function error(v?: any): void {
     log(console.error, v);
+}
+
+export function input(v?: any): void {
+    let text = "";
+    if(typeof v === "string") {
+        text = v;
+    } else if(v instanceof Variable) {
+        text = v.value;
+    } else {
+        text = v.toString();
+    }
+    return prompt(text);
 }
